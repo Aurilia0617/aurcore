35c8fc28453f67be6822ad868394b0409ef46ac7
Aurilia0617 <aurilia.ict@gmail.com>
2024-05-12 22:11:42 +0800

    fix bugs

--- define/class.lua
+++ define/class.lua
@@ -1,10 +1,7 @@
 -- 当类定义在三方库和内置库之间切换时才需要改这里的require路径
 return {
     _CONST = { _name = "library" }, -- 适配混入表类型
-    get = function(_)
-        return require("aurcore.define.third_party.class.interfaces")
-    end,
     check = function(_, classLib)
-        return require("aurcore.define.third_party.class.check")(classLib)
+        return require("aurcore.define.third_party.class.interfaces")(classLib)
     end
 }
--- define/init.lua
+++ define/init.lua
@@ -4,7 +4,7 @@ local define = Types:new_obj("define")
 -- class 比较底层，因此单开文件特别供应（防止循环引用），不放在这
 
 function define:check_unit_test(UnitTestLib)
-    return require("aurcore.define.third_party.unit_test.check")(UnitTestLib)
+    return require("aurcore.define.third_party.unit_test.interfaces")(UnitTestLib)
 end
 
 return define
